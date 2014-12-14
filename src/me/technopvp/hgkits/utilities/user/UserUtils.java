600b7d05457493b11f32272e9bf715ac0bff12e6
TechnoPvP <adamware99@hotmail.com>
2014-12-14 03:20:08 -0500

    Changed user file

--- src/me/technopvp/hgkits/utilities/user/UserUtils.java
+++ src/me/technopvp/hgkits/utilities/user/UserUtils.java
@@ -9,10 +9,27 @@ public class UserUtils extends User {
 		super(plugin, user);
 	}
 
+	/**
+	 * Set the user's kit in the user file.
+	 *
+	 * @param user
+	 *            - The user that will be adding the kit to in there user file.
+	 * @param kitName
+	 *            - The name of the kit you will be adding.
+	 */
+
 	public static void setUserKit(String user, String kitName) {
 		getUser(user).getPlayerConfig().getConfig().set("Kit", kitName);
 		getUser(user).getPlayerConfig().savePlayerConfig();
 	}
+
+	/**
+	 * Remove a users kit from there user file.
+	 *
+	 * @param user
+	 *            - The user that you will be removing there kit.
+	 *
+	 */
 
 	public static void removeUserKit(String user) {
 		if (userHasKit(user) == true) {
@@ -21,9 +38,26 @@ public class UserUtils extends User {
 		}
 	}
 
+	/**
+	 *
+	 * @param user
+	 *            - The user from the string.
+	 * @return The user's kit from the configeration file.
+	 */
+
 	public static String getUserKit(String user) {
 		return getUser(user).getPlayerConfig().getConfig().getString("Kit");
 	}
+
+	/**
+	 *
+	 * Check if a user has a kit. <br>
+	 * It will return the value depending on the condition.
+	 *
+	 * @param user
+	 *            - The user you will be checking
+	 * @return the condition on if they have the kit.
+	 */
 
 	public static boolean userHasKit(String user) {
 		return (getUserConfig(user).contains("Kit"));
